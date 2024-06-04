4ad55beec6bfee8a4fff846dd3127ba93624f080
MateusAndrade <mateus.andrade47@outlook.com>
2024-06-04 20:58:29 +0200

    chore(file): removing duplicated component due to windows issues

--- components/icon/WeatherIcon/__tests__/WeatherIcon.spec.tsx
+++ components/icon/WeatherIcon/__tests__/WeatherIcon.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WeatherIcon } from '../../WeatherIcon';
+import { WeatherIcon } from '../WeatherIcon';
 
 import { render } from '@testing-library/react-native';
 
