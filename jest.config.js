9d358b3cee07d3f7d6611caed584047728bae124
MateusAndrade <mateus.andrade47@outlook.com>
2024-06-04 20:55:55 +0200

    test(jest): fixing jest config to run components with theme provider

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|@gluestack-ui/*|@gluestack-style/*|@legendapp/*|react-native-svg)',
+  ],
+  testMatch: ['**/*.spec.ts', '**/*.spec.tsx'],
 };
